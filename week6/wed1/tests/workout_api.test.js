42068626a20744fdfeb3b755e062e5b59def5f0e
Renan Horuz <renanh@metropolia.fi>
2026-02-25 10:43:27 +0200

    w6 mon both and wed1

---
+++ week6/wed1/tests/workout_api.test.js
@@ -0,0 +1,129 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const Workout = require("../models/workoutModel");
+
+const initialWorkouts = [
+  {
+    title: "test workout 1",
+    reps: 11,
+    load: 101,
+  },
+  {
+    title: "test workout 2",
+    reps: 12,
+    load: 102,
+  },
+];
+
+const workoutsInDb = async () => {
+  const workouts = await Workout.find({});
+  return workouts.map((workout) => workout.toJSON());
+};
+
+beforeEach(async () => {
+  await Workout.deleteMany({});
+  let workoutObject = new Workout(initialWorkouts[0]);
+  await workoutObject.save();
+  workoutObject = new Workout(initialWorkouts[1]);
+  await workoutObject.save();
+});
+
+describe("GET /api/workouts", () => {
+  it("should return all workouts", async () => {
+    console.log("entered test");
+    const response = await api.get("/api/workouts");
+
+    expect(response.body).toHaveLength(initialWorkouts.length);
+  });
+
+  it("should return a specific workout, that is within the returned workouts", async () => {
+    console.log("entered test");
+    const response = await api.get("/api/workouts");
+
+    const contents = response.body.map((r) => r.title);
+    expect(contents).toContain("test workout 2");
+  });
+
+  it("should return Workouts as json", async () => {
+    console.log("entered test");
+    await api
+      .get("/api/workouts")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+});
+
+describe("POST /api/workouts", () => {
+  describe("when payload is valid", () => {
+    it("should add a new workout successfully", async () => {
+      console.log("entered test");
+      const newWorkout = {
+        title: "test workout x",
+        reps: 19,
+        load: 109,
+      };
+      await api.post("/api/workouts").send(newWorkout).expect(201);
+    });
+
+    it("should add a valid workout", async () => {
+      console.log("entered test");
+      const newWorkout = {
+        title: "Situps",
+        reps: 25,
+        load: 10,
+      };
+
+      await api
+        .post("/api/workouts")
+        .send(newWorkout)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+      const response = await api.get("/api/workouts");
+
+      const contents = response.body.map((r) => r.title);
+
+      expect(response.body).toHaveLength(initialWorkouts.length + 1);
+      expect(contents).toContain("Situps");
+    });
+  });
+  describe("when payload is invalid", () => {
+    it("should fail to add aworkout without a title", async () => {
+      console.log("entered test");
+      const newWorkout = {
+        reps: 23,
+      };
+
+      await api.post("/api/workouts").send(newWorkout).expect(400);
+
+      const response = await api.get("/api/workouts");
+
+      expect(response.body).toHaveLength(initialWorkouts.length);
+    });
+  });
+});
+
+describe("DELETE /api/workouts/:id", () => {
+  describe("when the id is valid", () => {
+    it("should succeed with status code 204 if id is valid", async () => {
+      const workoutsAtStart = await workoutsInDb();
+      const workoutToDelete = workoutsAtStart[0];
+
+      await api.delete(`/api/workouts/${workoutToDelete.id}`).expect(204);
+
+      const workoutsAtEnd = await workoutsInDb();
+      expect(workoutsAtEnd).toHaveLength(initialWorkouts.length - 1);
+
+      const contents = workoutsAtEnd.map((r) => r.title);
+      expect(contents).not.toContain(workoutToDelete.title);
+
+      // expect(true).toBe(true);
+    });
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
